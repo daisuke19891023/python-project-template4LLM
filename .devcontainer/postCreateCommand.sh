388758a5a83f4edee535d55ecce4ebfc56274608
Daisuke19891023 <okadaih11023@gmail.com>
2024-08-12 23:12:13 +0900

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,5 +5,10 @@ echo "START Install"
 
 sudo chown -R vscode:vscode .
 poetry config virtualenvs.in-project true
-poetry install
+poetry install --with test,docs,dev,typing
+
+
+# add PYTHONPATH
+echo 'export PYTHONPATH="$PWD/mymodule:$PYTHONPATH"' >> .venv/bin/activate
+source .venv/bin/activate
 echo "FINISH Install"
